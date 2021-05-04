95d4e100190d0857ce7254221acab7abc31fc898
haider <haideralifaheem@hotmail.com>
2021-05-04 20:03:45 +0200

    Project Started and MongoDb dependancy added

---
+++ src/main/java/com/fields/weather/fieldsweather/FieldsweatherApplication.java
@@ -0,0 +1,13 @@
+package com.fields.weather.fieldsweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldsweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldsweatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fields/weather/fieldsweather/FieldsweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fields.weather.fieldsweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldsweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
