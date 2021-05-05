71cc1f9ce56f7bf30043b640c0c572ac3f40dd8e
haider <haideralifaheem@hotmail.com>
2021-05-05 09:57:45 +0200

    Issue with Swagger pattren

--- src/main/java/com/fields/weather/fieldsweather/Model/Field.java
+++ src/main/java/com/fields/weather/fieldsweather/Model/Field.java
@@ -12,7 +12,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Field")
+@Document(collection = "FieldTable")
 public class Field implements Serializable {
 
     private static final long serialVersionUID = 1L;
